4aa38aa494cc4e4faf7b03f73690c8f5c4a0da63
Gustavo Silva <gustavofsilva@email.com>
2020-03-03 16:28:51 -0300

    Add react-route-dom and create new route to products

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { BrowserRouter, Switch, Route, Router} from 'react-router-dom';
+import Product from './pages/product';
+
+import Main from './pages/main';
+
+const Routes = () =>(
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Main} />
+            <Route path='/products/:id' component={Product} />
+        </Switch>
+    
+    </BrowserRouter>
+);
+
+export default Routes;
