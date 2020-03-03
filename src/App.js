e5a11db59f74eb224d0e68c18057f89dda161f42
Gustavo Silva <gustavofsilva@email.com>
2020-03-03 11:21:01 -0300

    Add the products using api with AXIOS

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Header from './components/Header';
+
 import './styles.css';
+
+import Header from './components/Header';
+import Main from './pages/main'
+
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
