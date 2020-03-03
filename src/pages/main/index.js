49a8bf72f933cf6f0447043362e1aaf4945f0433
Gustavo Silva <gustavofsilva@email.com>
2020-03-03 16:10:58 -0300

    Add nextPage and prevPage

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -5,19 +5,42 @@ import './styles.css';
 export default class Main extends Component {
     state = {
         products: [],
+        productInfo: {},
+        page: 1
     }
 
     componentDidMount(){
         this.loadProducts();
     }
 
-    loadProducts = async () =>{
-        const response = await api.get('/products');
+    loadProducts = async (page = 1) =>{
+        const response = await api.get(`/products?page=${page}`);
         
-        this.setState({products: response.data.docs})
+        const { docs, ...productInfo} = response.data;
+
+        
+
+        this.setState({products: docs, productInfo, page})
         
     }
 
+    prevPage = () =>{
+        const { page, productInfo} = this.state;
+
+        if(productInfo.page === 1) return;
+        
+        const pageNumber = page - 1;
+        this.loadProducts(pageNumber)
+    }
+
+    nextPage = () =>{
+        const { page, productInfo} = this.state;
+
+        if(page === productInfo.pages) return;
+        const pageNumber = page + 1;
+
+        this.loadProducts(pageNumber);
+    }
     
 
     render(){
@@ -32,7 +55,13 @@ export default class Main extends Component {
                     <a href="">Acessar</a>
                 </article>
             ))}
+            <div className="actions">
+                <button disabled={this.state.page ===1} onClick={this.prevPage}>Anterior</button>
+                <button disabled={this.state.page === this.state.productInfo.pages} onClick={this.nextPage}>Próximo</button>
             </div>
+
+            </div>
+
         )
     }
 }
